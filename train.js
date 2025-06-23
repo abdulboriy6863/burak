6c67dae11d9162dd9d3ca18569bb9f1317e1bc58
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-06-23 06:29:15 -0700

    FEAT: complated task H

--- train.js
+++ train.js
@@ -1,10 +1,24 @@
 //TASK G
-function getHighestIndex(a) {
-  // return Math.max(...a);
-  const maxValue = Math.max(...a);
-  const maxIndex = a.indexOf(maxValue);
-  console.log(
-    `Bu ${maxValue} soni arraynig tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi ${maxIndex} qaytadi`
-  );
+// function getHighestIndex(a) {
+//   // return Math.max(...a);
+//   const maxValue = Math.max(...a);
+//   const maxIndex = a.indexOf(maxValue);
+//   console.log(
+//     `Bu ${maxValue} soni arraynig tarkibidagi birinchi eng katta son hisobladi va bizga uning indeksi ${maxIndex} qaytadi`
+//   );
+// }
+// getHighestIndex([40, 24, 15]);
+
+//TASK H
+function getPositive(a) {
+  let result = a
+    .filter((eleEle) => {
+      return eleEle > 0;
+    })
+    .join("");
+
+  console.log(result);
+  console.log(typeof result);
+  // console.log('"' + result + '"');
 }
-getHighestIndex([40, 24, 125]);
+getPositive([1, -4, -30, 2]);
