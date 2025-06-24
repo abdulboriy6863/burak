7b4ffe261474e2fa687ea5e520841be3aac52d5b
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-06-24 06:46:06 -0700

    feat: integration of environmental variable

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,5 @@
-console.log("EXUCUTED!");
+import dotenv from "dotenv";
+dotenv.config();
 
-import moment from "moment";
-
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
-
-const person: string = "Jack";
-const count: number = 100;
+// console.log("PORT:", process.env.PORT);
+// console.log("MONGO_URL:", process.env.MONGO_URL);
--- train.js
+++ train.js
@@ -13,12 +13,25 @@
 function getPositive(a) {
   let result = a
     .filter((eleEle) => {
-      return eleEle > 0;
+      return eleEle >= 0;
     })
     .join("");
 
   console.log(result);
   console.log(typeof result);
-  // console.log('"' + result + '"');
+  // console.log(`"${result}"`);
 }
 getPositive([1, -4, -30, 2]);
+
+//Loyihamizni togridan togri ishlatyotgan packagelarimizni togridan togri (dependencies)ga install qilamiz
+//Loyihamizni faqatgina DEVELOP jaroyonida ishlatadigon package larimizni (devependencies)ga install qilamiz
+
+/*MODULE JS sintaksisi
+import moment from 'moment';
+*/
+
+/*COMMON JS sintaksisi
+const moment = require ('moment');
+*/
+
+//Environmental variable file
