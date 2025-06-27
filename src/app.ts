3a1a28fe3e0e8fbddf3a03d9b0b45ad1141bbfbc
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-06-26 19:37:22 -0700

    fix:modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 /* 1-ENTRANCE*/
 const app = express();
 // console.log("__dirname:", __dirname);
@@ -13,6 +14,9 @@ app.use(express.json());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 /* 4-ROUTERS*/
-app.use("/", router); // middleware design pattern
+app.use("/admin", routerAdmin); // SSR: EJS
+app.use("/", router); // SPA: REACT
+// middleware design pattern
+//Burak backend serverni REACT loyihamizga (rest api ) server sifatida ishlatamiz
 
 export default app;
