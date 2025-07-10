ac160d88f3303c5e063d0ef72a63f4eb0cff3010
abdulboriy6863 <abdulboriy6863@gmail.com>
2025-07-10 11:22:31 -0700

    feat: createNewProduct business logic by restaurant member

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import Productservice from "../models/Product.service";
 import { AdminRequest } from "../libs/types/member";
+import { ProductInput } from "../libs/types/product";
 
 const productService = new Productservice();
 
 const productController: T = {};
+
+/* SPA */
+
+/* SSR */
+
 productController.getAllProducts = async (req: Request, res: Response) => {
   try {
     console.log("getAllProducts"); // log qilishimizni sababi => requestimiz backandga kirib keladimi yana bu (loging standarti)
@@ -20,14 +26,35 @@ productController.getAllProducts = async (req: Request, res: Response) => {
   }
 };
 
-productController.createNewProduct = async (req: Request, res: Response) => {
+productController.createNewProduct = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     console.log("createNewProduct"); // log qilishimizni sababi => requestimiz backandga kirib keladimi yana bu (loging standarti)
-    res.send("DONE!");
+
+    // console.log("req.files", req.files);
+
+    if (!req.files?.length)
+      throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+    const data: ProductInput = req.body;
+    data.productImages = req.files?.map((ele) => {
+      return ele.path;
+    });
+
+    await productService.createNewProduct(data);
+    //shuyer
+    res.send(
+      `<script> alert ("Sucessful creation!") window.location.replace('/admin/product/all')</script>,`
+    );
   } catch (err) {
     console.log("Error, createNewProduct", err);
-    if (err instanceof Errors) res.status(err.code).json(err);
-    else res.status(Errors.standard.code).json(Errors.standard);
+    const message =
+      err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+    //messageyimzni qiymati biz hosil qilgan class dagi errorga dahildor bolsa unda bizni hatoligimzni olib ber aksxolda umimiy hatolikdan something went wrongni olib ber
+    res.send(
+      `<script> alert ("${message}") window.location.replace('/admin/product/all')</script>,`
+    );
   }
 };
 
---
+++ src/libs/types/product.ts
@@ -0,0 +1,34 @@
+import { ObjectId } from "mongoose";
+import {
+  ProductCollection,
+  ProductSize,
+  ProductStatus,
+} from "../enums/product.enum";
+
+export interface Product {
+  _id: ObjectId;
+  ProductStatus: ProductStatus;
+  productCollection: ProductCollection;
+  productName: string;
+  productPrice: number;
+  productLeftCount: number;
+  productSize: ProductSize;
+  productVolume: number;
+  productDesc?: string;
+  productImages: string[];
+  productViews: number;
+}
+
+export interface ProductInput {
+  productImages: string[];
+  ProductStatus?: ProductStatus;
+  productCollection: ProductCollection;
+  productName: string;
+  productPrice: number;
+  productLeftCount: number;
+  productSize?: ProductSize;
+  productVolume?: number;
+  productDesc?: string;
+  productImage?: string[];
+  productViews?: number;
+}
